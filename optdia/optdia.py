c8a6318770fbe0a03e5015b9a1ac92211a689a03
kazuma <>
2026-05-31 07:00:00 +0000

    メインウィンドウの左端にサイドバーを設置
    FossilOrigin-Name: 13a02b5eaa5dc484b8e28be579489b7e29180b4406b0e5c68a3b1942f3247ebb

--- optdia/optdia.py
+++ optdia/optdia.py
@@ -2,7 +2,8 @@
 # coding: utf-8
 
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton
 from project import OptDiaProject, load_project
 
 APP_NAME = "OptDia"
@@ -18,6 +19,61 @@ class MainWindow(QMainWindow):
         # 初期タイトルと初期サイズ
         self.setWindowTitle(f"{APP_NAME} v{__version__}")
         self.resize(960, 640)
+
+        # セントラルウィジェットの設定
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        
+        # メインとなる水平レイアウト
+        main_layout = QHBoxLayout(central_widget)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
+
+        # 左側のサイドバーウィジェット (幅240px固定)
+        sidebar = QWidget()
+        sidebar.setFixedWidth(240)
+        sidebar.setStyleSheet("background-color: #f7f7f7; border-right: 1px solid #dddddd;")
+        
+        sidebar_layout = QVBoxLayout(sidebar)
+        sidebar_layout.setContentsMargins(0, 0, 0, 0)
+        sidebar_layout.setSpacing(0)
+
+        # ボタンの共通スタイル定義
+        button_style = """
+            QPushButton {
+                border: none;
+                text-align: left;
+                text-decoration: underline;
+                padding-left: 15px;
+                font-size: 14px;
+                background-color: transparent;
+            }
+            QPushButton:hover {
+                background-color: #eeeeee;
+            }
+        """
+
+        # 1つ目のボタン: 路線・駅情報
+        self.btn_lines = QPushButton("路線・駅情報")
+        self.btn_lines.setFixedHeight(50)
+        self.btn_lines.setStyleSheet(button_style)
+        sidebar_layout.addWidget(self.btn_lines)
+
+        # 2つ目のボタン: 種別情報
+        self.btn_types = QPushButton("種別情報")
+        self.btn_types.setFixedHeight(50)
+        self.btn_types.setStyleSheet(button_style)
+        sidebar_layout.addWidget(self.btn_types)
+
+        # 下部に伸縮スペースを入れてボタンを上部に寄せる
+        sidebar_layout.addStretch()
+
+        # レイアウトにサイドバーを追加
+        main_layout.addWidget(sidebar)
+        
+        # 右側のコンテンツ表示エリア (将来の拡張用)
+        self.content_container = QWidget()
+        main_layout.addWidget(self.content_container, stretch=1)
 
 
 # アプリ起動処理
