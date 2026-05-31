9d08e27f38f0451adcd77bd29fd1ef884cf4c390
kazuma <>
2026-05-31 06:00:00 +0000

    アプリ起動時にプロジェクトデータを読み込むように
    FossilOrigin-Name: a90bf700a9770553445f15317cd9186419e25fa154db42cfed3a11cce83762fd

--- optdia/optdia.py
+++ optdia/optdia.py
@@ -3,7 +3,7 @@
 
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-
+from project import OptDiaProject, load_project
 
 APP_NAME = "OptDia"
 __version__ = "26.06-1"
@@ -11,8 +11,9 @@ __version__ = "26.06-1"
 
 # メインウィンドウ
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, project: OptDiaProject):
         super().__init__()
+        self.project = project
 
         # 初期タイトルと初期サイズ
         self.setWindowTitle(f"{APP_NAME} v{__version__}")
@@ -23,7 +24,14 @@ class MainWindow(QMainWindow):
 def main():
     app = QApplication(sys.argv)
 
-    window = MainWindow()
+    # コマンドライン引数でファイルパスが指定されている場合はロード、
+    # そうでない場合は新規プロジェクトを生成
+    if len(sys.argv) > 1:
+        project = load_project(sys.argv[1])
+    else:
+        project = OptDiaProject()
+
+    window = MainWindow(project)
 
     window.show()
     sys.exit(app.exec())
